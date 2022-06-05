a8d084781b5c4c00ae94921f1139776e378b5c6a
BORED <105173245+MicahBill@users.noreply.github.com>
2022-06-05 19:24:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from pytube import YouTube
 from io import BytesIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
+app.config['SECRET_KEY'] = "654c0fb4969af9d5s6a9b3edcdc7051b"
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if request.method == "POST":
-        session['link'] = request.form.get('https://layladownloader.herokuapp.com/')
+        session['link'] = request.form.get('url')
         try:
             url = YouTube(session['link'])
             url.check_availability()
         except:
             return render_template("error.html")
-        return render_template("download.html", https://layladownloader.herokuapp.com/ = https://layladownloader.herokuapp.com/)
+        return render_template("download.html", url = url)
     return render_template("home.html")
 
 @app.route("/download", methods = ["GET", "POST"])
@@ -26,7 +26,7 @@ def download_video():
         video = url.streams.get_by_itag(itag)
         video.stream_to_buffer(buffer)
         buffer.seek(0)
-        return send_file(buffer, as_attachment=True, download_name="Video - YT2Video.mp4", mimetype="video/mp4")
+        return send_file(buffer, as_attachment=True, download_name="Video - SeniorVideo.mp4", mimetype="video/mp4")
     return redirect(url_for("home"))
 
 if __name__ == '__main__':
