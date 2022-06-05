6d7d38d8f9f4bbdeae7cdb0b9749122caaaab5f3
BORED <105173245+MicahBill@users.noreply.github.com>
2022-06-05 19:23:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if request.method == "POST":
-        session['link'] = request.form.get('url')
+        session['link'] = request.form.get('https://layladownloader.herokuapp.com/')
         try:
             url = YouTube(session['link'])
             url.check_availability()
         except:
             return render_template("error.html")
-        return render_template("download.html")
+        return render_template("download.html", https://layladownloader.herokuapp.com/ = https://layladownloader.herokuapp.com/)
     return render_template("home.html")
 
 @app.route("/download", methods = ["GET", "POST"])
